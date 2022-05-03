b7384ca9ae6e556c6fb28b9207bfe585f47a7d16
Quinn Henry <19qhenry@gmail.com>
2022-05-02 22:08:25 -0400

    Add a few GitHub files

---
+++ Terraform/GitHub/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+    token = var.github_token
+}
---
+++ Terraform/GitHub/variables.tf
@@ -0,0 +1,5 @@
+variable "github_token" {
+    type = string
+    sensitive = true
+    description = "Token used to authenticate to GitHub"
+}
