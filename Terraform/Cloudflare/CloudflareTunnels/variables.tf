dd439f01f93df261484fded4a3c4c44cb0597092
Quinn Henry <78760148+TheQuib@users.noreply.github.com>
2023-06-23 12:57:48 -0400

    🍎 Add tf_cloud_token variable

--- Terraform/Cloudflare/CloudflareTunnels/variables.tf
+++ Terraform/Cloudflare/CloudflareTunnels/variables.tf
@@ -7,3 +7,8 @@ variable "tf_cloud_workspace" {
     type = string
     description = "Terraform Cloud workspace name"
 }
+
+variable "tf_cloud_token" {
+    type = string
+    description = "Terraform Cloud token value"
+}
